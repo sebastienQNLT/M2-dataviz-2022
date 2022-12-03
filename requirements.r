ff2e8093eded3c188b07cf4f49899112e8d405c4
sebastienQNLT <45787958+sebastienQNLT@users.noreply.github.com>
2022-12-03 14:38:29 +0100

    Add files via upload

---
+++ requirements.r
@@ -0,0 +1,7 @@
+# install requirements
+install.packages("dplyr")
+install.packages("plotly")
+install.packages("skimr")
+install.packages("flexdashboard")
+
+url="https://github.com/sebastienQNLT/M2-dataviz-2022/raw/main/fifadataset.Rdata"
